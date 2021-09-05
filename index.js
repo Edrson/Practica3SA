91732648dc0674066aaea6e8dfae635f63f5bfd1
Edrson <ederson.ramirez87@gmail.com>
2021-09-04 23:18:37 -0600

    update..

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-// importando el marco del nodo 
-var express = require ('express'); 
- 
-var app = express ();
-// Responde con "hola mundo" a las solicitudes que llegan a nuestra raíz "/" 
-app.get ('/', function (req, res) { 
-res.send ('hola mundo'); 
+/** @format */
+
+// importando el marco del nodo
+var express = require("express");
+
+var app = express();
+// Responde con "hola mundo" a las solicitudes que llegan a nuestra raíz "/"
+app.get("/", function (req, res) {
+  res.send("hola mundo.");
 });
-// escucha el puerto 3000 por defecto 
-console.log('aplicacion escuchando en el puerto 3000...')
-app.listen (process.env.PORT || 3000); 
- 
+// escucha el puerto 3000 por defecto
+console.log("aplicacion escuchando en el puerto 3000...");
+app.listen(process.env.PORT || 3000);
+
 module.exports = app;
